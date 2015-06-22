706c36b39900858110f2ae02c3000b54a69016ec
ChihM <s102062111@m102.nthu.edu.tw>
2015-06-22 22:33:44 +0800

    add LED output for FPGA

--- Lab4_2.v
+++ Lab4_2.v
@@ -1,6 +1,8 @@
-module final(LED, INPUT);
-	input INPUT;
-	output LED;
-	assign LED = INPUT;
+module final(LED, INPUT, LED_2);
+	input [2:0] INPUT;
+	output [2:0] LED_2;
+	output [2:0] LED;
 	
+	assign LED = INPUT;
+	assign LED_2 = INPUT;
 endmodule
